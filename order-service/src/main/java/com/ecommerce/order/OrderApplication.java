c89e711c6d2728530886e64838343b777025077d
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-02-28 01:04:39 -0300

    infra (order-service): adds new project order-service

---
+++ order-service/src/main/java/com/ecommerce/order/OrderApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
