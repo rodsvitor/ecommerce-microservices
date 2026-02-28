692cb8a5233fff4e9973c7556be5e4ee8e140ee9
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-02-28 01:10:43 -0300

    dev (product-service): creates port for event publisher and improves dto names

--- product-service/src/main/java/com/ecommerce/product/application/usecase/CreateProductUseCase.java
+++ product-service/src/main/java/com/ecommerce/product/application/usecase/CreateProductUseCase.java
@@ -1,12 +1,14 @@
 package com.ecommerce.product.application.usecase;
 
 import com.ecommerce.product.application.mapper.ProductMapperDTO;
+import com.ecommerce.product.application.port.ProductEventPublisher;
 import com.ecommerce.product.domain.model.Product;
 import com.ecommerce.product.domain.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 @Service
 @RequiredArgsConstructor
@@ -14,19 +16,21 @@ public class CreateProductUseCase {
 
   private final ProductRepository productRepository;
   private final ProductMapperDTO mapper;
+  private final ProductEventPublisher eventPublisher;
 
-  public CreateProductResponse execute(CreateProductRequest request) {
+  public ProductCreatedResponse execute(CreateProductRequest request) {
 
     Product product = mapper.toDomain(request);
+    product.setCreatedAt(Instant.now());
 
     product = productRepository.save(product);
+    eventPublisher.publishProductCreated(product);
 
     return mapper.toCreationResponse(product);
-
   }
 
   public record CreateProductRequest(String name, BigDecimal price) {}
-  public record CreateProductResponse(Long id, String name, BigDecimal price) {}
+  public record ProductCreatedResponse(Long id, String name, BigDecimal price) {}
 
 
 }
