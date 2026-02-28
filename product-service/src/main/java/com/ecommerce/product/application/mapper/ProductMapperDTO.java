1551445ba494eec2ec1d6ac83c8e338c06b2b89d
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-02-28 01:12:57 -0300

    dev (product-service): improves dto names in product mapper dto

--- product-service/src/main/java/com/ecommerce/product/application/mapper/ProductMapperDTO.java
+++ product-service/src/main/java/com/ecommerce/product/application/mapper/ProductMapperDTO.java
@@ -2,6 +2,7 @@ package com.ecommerce.product.application.mapper;
 
 import com.ecommerce.product.application.usecase.CreateProductUseCase;
 import com.ecommerce.product.application.usecase.ProductQueryService;
+import com.ecommerce.product.application.usecase.UpdateProductUseCase.UpdatedProductResponse;
 import com.ecommerce.product.domain.model.Product;
 import org.mapstruct.Mapper;
 
@@ -10,7 +11,9 @@ public interface ProductMapperDTO {
 
   Product toDomain(CreateProductUseCase.CreateProductRequest product);
 
-  CreateProductUseCase.CreateProductResponse toCreationResponse(Product product);
+  CreateProductUseCase.ProductCreatedResponse toCreationResponse(Product product);
+
+  UpdatedProductResponse toUpdatedProductResponse(Product product);
 
   ProductQueryService.QueriedProductResponse toQueriedResponse(Product product);
 
