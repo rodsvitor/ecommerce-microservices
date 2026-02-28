968aa2c36f811ff492b93498b1b5780c1bd2ba7e
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-02-28 01:13:42 -0300

    dev (product-service): adds methods delete and update to controller

--- product-service/src/main/java/com/ecommerce/product/entrypoint/controller/ProductController.java
+++ product-service/src/main/java/com/ecommerce/product/entrypoint/controller/ProductController.java
@@ -2,8 +2,13 @@ package com.ecommerce.product.entrypoint.controller;
 
 import com.ecommerce.product.application.usecase.CreateProductUseCase;
 import com.ecommerce.product.application.usecase.CreateProductUseCase.CreateProductRequest;
-import com.ecommerce.product.application.usecase.CreateProductUseCase.CreateProductResponse;
+import com.ecommerce.product.application.usecase.CreateProductUseCase.ProductCreatedResponse;
+import com.ecommerce.product.application.usecase.DeleteProductUseCase;
 import com.ecommerce.product.application.usecase.ProductQueryService;
+import com.ecommerce.product.application.usecase.ProductQueryService.QueriedProductResponse;
+import com.ecommerce.product.application.usecase.UpdateProductUseCase;
+import com.ecommerce.product.application.usecase.UpdateProductUseCase.UpdateProductRequest;
+import com.ecommerce.product.application.usecase.UpdateProductUseCase.UpdatedProductResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,21 +22,34 @@ import java.util.List;
 public class ProductController {
 
   private final CreateProductUseCase createProductUseCase;
+  private final UpdateProductUseCase updateProductUseCase;
+  private final DeleteProductUseCase deleteProductUseCase;
   private final ProductQueryService productQueryService;
 
   @PostMapping
-  public ResponseEntity<CreateProductResponse> create(@RequestBody CreateProductRequest productRequest) {
+  public ResponseEntity<ProductCreatedResponse> create(@RequestBody CreateProductRequest productRequest) {
     return new ResponseEntity<>(createProductUseCase.execute(productRequest), HttpStatus.CREATED);
   }
 
   @GetMapping
-  public ResponseEntity<List<ProductQueryService.QueriedProductResponse>> listAll() {
+  public ResponseEntity<List<QueriedProductResponse>> listAll() {
     return ResponseEntity.ok(productQueryService.listAll());
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<ProductQueryService.QueriedProductResponse> findById(@PathVariable Long id) {
+  public ResponseEntity<QueriedProductResponse> findById(@PathVariable Long id) {
     return ResponseEntity.ok(productQueryService.findById(id));
+  }
+
+  @PutMapping
+  public ResponseEntity<UpdatedProductResponse> update(@RequestBody UpdateProductRequest updateProductRequest) {
+    return ResponseEntity.ok(updateProductUseCase.execute(updateProductRequest));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    deleteProductUseCase.execute(id);
+    return ResponseEntity.noContent().build();
   }
 
 }
