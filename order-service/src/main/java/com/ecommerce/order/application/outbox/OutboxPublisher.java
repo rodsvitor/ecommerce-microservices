58cddc9df3b7318e4f63998d1d39ac7ec01a74d4
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-03-11 23:24:26 -0300

    dev (order-service): defines scheduled publisher for OutboxEvent

---
+++ order-service/src/main/java/com/ecommerce/order/application/outbox/OutboxPublisher.java
@@ -0,0 +1,37 @@
+package com.ecommerce.order.application.outbox;
+
+import com.ecommerce.order.application.port.OrderEventPublisher;
+import com.ecommerce.order.domain.outbox.OutboxEvent;
+import com.ecommerce.order.domain.outbox.OutboxRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OutboxPublisher {
+
+  private final OutboxRepository outboxRepository;
+  private final OrderEventPublisher publisher;
+
+  @Scheduled(fixedDelay = 10_000)
+  public void publishEvents() {
+
+    // TODO study about need of transaction here.
+    List<OutboxEvent> events = outboxRepository.findByPublishedFalse();
+
+    for (OutboxEvent event : events) {
+      publisher.publish(event);
+      event.setPublished(true);
+      outboxRepository.save(event);
+
+      log.info("Outbox event published: {}", event.getId());
+    }
+
+  }
+
+}
