4b85e6562f199b1e6b2b35cd169a834ee6a3c2d2
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-03-11 23:31:29 -0300

    dev (order-service): modifies CreateOrderUseCase to saves order as outbox event, not publishing event immediately anymore.

--- order-service/src/main/java/com/ecommerce/order/application/usecase/CreateOrderUseCase.java
+++ order-service/src/main/java/com/ecommerce/order/application/usecase/CreateOrderUseCase.java
@@ -3,20 +3,28 @@ package com.ecommerce.order.application.usecase;
 import com.ecommerce.order.application.dto.request.CreateOrderRequest;
 import com.ecommerce.order.application.dto.request.OrderItemRequest;
 import com.ecommerce.order.application.dto.response.OrderResponse;
+import com.ecommerce.order.application.event.OrderCreatedEvent;
 import com.ecommerce.order.application.mapper.OrderMapperDTO;
-import com.ecommerce.order.application.port.OrderEventPublisher;
+import com.ecommerce.order.application.port.EventSerializer;
 import com.ecommerce.order.domain.model.Order;
 import com.ecommerce.order.domain.model.OrderItem;
 import com.ecommerce.order.domain.model.OrderStatus;
+import com.ecommerce.order.domain.outbox.OutboxEvent;
+import com.ecommerce.order.domain.outbox.OutboxRepository;
 import com.ecommerce.order.domain.repository.OrderRepository;
 import com.ecommerce.order.domain.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
+
+import static com.ecommerce.order.application.mapper.OrderMapperEvent.toOrderCreatedEvent;
+import static com.ecommerce.order.domain.outbox.AggregateType.ORDER;
+import static com.ecommerce.order.domain.outbox.EventType.ORDER_CREATED;
 
 @Service
 @RequiredArgsConstructor
@@ -24,24 +32,50 @@ public class CreateOrderUseCase {
 
   private final OrderRepository orderRepository;
   private final ProductRepository productRepository;
-  private final OrderMapperDTO orderMapperDTO;
-  private final OrderEventPublisher eventPublisher;
+  private final OutboxRepository outboxRepository;
 
+  private final EventSerializer eventSerializer;
+  private final OrderMapperDTO orderMapperDTO;
+
+  @Transactional
   public OrderResponse execute(CreateOrderRequest request) {
+
+    var order = buildOrder(request);
+    order = orderRepository.save(order);
+
+    saveAsOutboxEvent(order);
+
+    return orderMapperDTO.toOrderCreatedResponse(order);
+
+  }
+
+  private void saveAsOutboxEvent(Order order) {
+
+    var orderCreatedEvent = toOrderCreatedEvent(order);
+    OutboxEvent outboxEvent = buildOutboxEvent(orderCreatedEvent);
+    outboxRepository.save(outboxEvent);
+
+  }
+
+  private Order buildOrder(CreateOrderRequest request) {
 
     List<OrderItem> items = request.items()
         .stream()
         .map(this::buildOrderItem)
         .toList();
 
-    var order = buildOrder(request, items);
+    BigDecimal totalAmount = items.stream()
+        .map(OrderItem::getSubtotal)
+        .reduce(BigDecimal.ZERO, BigDecimal::add);
 
-    order = orderRepository.save(order);
-
-    eventPublisher.publishOrderCreated(order);
-
-    return orderMapperDTO.toOrderCreatedResponse(order);
-
+    return Order.builder()
+        .id(UUID.randomUUID())
+        .userId(request.userId())
+        .items(items)
+        .totalAmount(totalAmount)
+        .status(OrderStatus.CREATED)
+        .createdAt(Instant.now())
+        .build();
   }
 
   private OrderItem buildOrderItem(OrderItemRequest item) {
@@ -61,20 +95,21 @@ public class CreateOrderUseCase {
         .build();
   }
 
-  private static Order buildOrder(CreateOrderRequest request, List<OrderItem> items) {
 
-    BigDecimal totalAmount = items.stream()
-        .map(OrderItem::getSubtotal)
-        .reduce(BigDecimal.ZERO, BigDecimal::add);
+  private OutboxEvent buildOutboxEvent(OrderCreatedEvent orderCreatedEvent) {
 
-    return Order.builder()
+    String payload = eventSerializer.serialize(orderCreatedEvent);
+
+    return OutboxEvent.builder()
         .id(UUID.randomUUID())
-        .userId(request.userId())
-        .items(items)
-        .totalAmount(totalAmount)
-        .status(OrderStatus.CREATED)
-        .createdAt(Instant.now())
+        .aggregateId(orderCreatedEvent.getOrderId().toString())
+        .aggregateType(ORDER)
+        .eventType(ORDER_CREATED)
+        .payload(payload)
+        .createdAt(orderCreatedEvent.getOrderCreatedAt())
+        .published(false)
         .build();
+
   }
 
 }
