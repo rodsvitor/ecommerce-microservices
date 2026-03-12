a62f5ad9b359dd8ed75c07eea66d24a2cf364a88
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-03-11 23:48:00 -0300

    dev (payment-service): adds some fields to Payment domain.

--- payment-service/src/main/java/com/ecommerce/payment/domain/model/Payment.java
+++ payment-service/src/main/java/com/ecommerce/payment/domain/model/Payment.java
@@ -3,6 +3,8 @@ package com.ecommerce.payment.domain.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.UUID;
 
 @Data
@@ -11,6 +13,8 @@ public class Payment {
 
   private UUID id;
   private UUID orderId;
+  private BigDecimal amount;
   private PaymentStatus status;
+  private Instant createdAt;
 
 }
