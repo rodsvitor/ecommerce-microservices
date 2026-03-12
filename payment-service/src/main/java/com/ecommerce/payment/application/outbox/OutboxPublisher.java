8588cf6e41e29a29029e78c1ce537ff1ba7fd351
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-03-11 23:53:33 -0300

    dev (payment-service): defines scheduled OutboxPublisher

---
+++ payment-service/src/main/java/com/ecommerce/payment/application/outbox/OutboxPublisher.java
@@ -0,0 +1,36 @@
+package com.ecommerce.payment.application.outbox;
+
+import com.ecommerce.payment.application.port.PaymentEventPublisher;
+import com.ecommerce.payment.domain.outbox.OutboxEvent;
+import com.ecommerce.payment.domain.outbox.OutboxRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OutboxPublisher {
+
+  private final OutboxRepository outboxRepository;
+  private final PaymentEventPublisher paymentEventPublisher;
+
+  @Scheduled(fixedDelay = 10_000)
+  public void publishEvents() {
+
+    var events = outboxRepository.findByPublishedFalse();
+
+    for (OutboxEvent event : events) {
+
+      paymentEventPublisher.publish(event);
+      event.setPublished(true);
+      outboxRepository.save(event);
+
+      log.info("Outbox event published: {}", event.getId());
+
+    }
+
+  }
+
+}
